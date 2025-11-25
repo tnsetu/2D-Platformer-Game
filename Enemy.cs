6fdee1d85b43f3c2bf4ad1ba6ced0e9c3eeab13f
Nsetu Nathan Tarimo <tnsetu@gmail.com>
2025-11-25 09:29:10 +0000

    Create Enemy.cs

---
+++ Enemy.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (GameManager.Instance != null)
+            {
+                GameManager.Instance.PlayerDied();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (GameManager.Instance != null)
+            {
+                GameManager.Instance.PlayerDied();
+            }
+        }
+    }
+}
